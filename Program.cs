0ab30eefdc1f32f112f0e49ecf68778bf4cd1546
gabrielrrbeiro-byte <gabrielrrbeiro@gmail.com>
2026-09-03 23:59:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RoverLunar
+{
+    internal class Program
+    {
+        static void InicializarRover()
+        {
+            Console.WriteLine("Sistemas do Rover iniciados!");
+            Console.WriteLine("Painéis solares: OK");
+            Console.WriteLine("Nível de bateria: 100%");
+        }
+
+        static void Main(string[] args)
+        {
+            InicializarRover();
+        }
+    }
+}
